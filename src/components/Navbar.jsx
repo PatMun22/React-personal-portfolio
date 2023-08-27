8bcf08d07132fabd5544e7cf98f6dc1214cac776
Patrick Munuku <wafulapatrick6@gmail.com>
2023-08-27 21:01:29 +0300

    Responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Navbar.css';
 
 const Navbar = () => {
+
+  const [activeLink, setActiveLink] = useState('#home');
+
   return (
     <div className='navbar'>
       <ul>
         <li>
-          <a href='#home'  className='active'>Home</a>
+          <a href='#home'  className={activeLink === '#home' ? 'active' : ''} onClick={() => setActiveLink('#home')}>Home</a>
         </li>
         <li>
-          <a href='#about'>About</a>
+          <a href='#about' className={activeLink === '#about' ? 'active' : ''} onClick={() => setActiveLink('#about')}>About</a>
         </li>
         <li>
-          <a href='#skills'>Skills</a>
+          <a href='#skills' className={activeLink === '#skills' ? 'active' : ''} onClick={() => setActiveLink('#skills')}>Skills</a>
         </li>
         <li>
-          <a href='#projects'>Projects</a>
+          <a href='#projects' className={activeLink === '#projects' ? 'active' : ''} onClick={() => setActiveLink('#projects')}>Projects</a>
         </li>
         <li>
-          <a href='#contacts'>Contacts</a>
+          <a href='#contacts' className={activeLink === '#contacts' ? 'active' : ''} onClick={() => setActiveLink('#contacts')}>Contacts</a>
         </li>
       </ul>
     </div>
