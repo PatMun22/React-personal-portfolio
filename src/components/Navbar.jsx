4935b53b44827e8a5d182c37f5e4c4a6f2f028b0
Patrick Munuku <wafulapatrick6@gmail.com>
2023-10-06 14:41:46 +0300

    Finetuning the Navbar on small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,6 @@ import { linkDatas } from "../Assets/data";
 import { GoHome } from "react-icons/go";
 import { AiOutlineUser, AiOutlineCopy, AiOutlineMessage } from "react-icons/ai";
 import { BsCodeSlash } from "react-icons/bs";
-import { motion, AnimatePresence } from "framer-motion";
-import { slideAnimation } from "../Assets/motion";
 
 const icons = [
   GoHome,
@@ -19,27 +17,25 @@ const Navbar = () => {
   const [activeLink, setActiveLink] = useState(0);
 
   return (
-    <AnimatePresence>
-      <motion.nav {...slideAnimation("right")} className="navbar">
-        <ul>
-          {linkDatas.map((linkData, index) => {
-            const Icon = icons[index];
-            return (
-              <li key={index}>
-                <a
-                  href={linkData.linkUrl}
-                  className={activeLink === index ? "active" : ""}
-                  onClick={() => setActiveLink(index)}
-                >
-                  <span>{linkData.linkName}</span>
-                  <Icon className="nav-icon" />
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </motion.nav>
-    </AnimatePresence>
+    <nav className="navbar">
+      <ul>
+        {linkDatas.map((linkData, index) => {
+          const Icon = icons[index];
+          return (
+            <li key={index}>
+              <a
+                href={linkData.linkUrl}
+                className={activeLink === index ? "active" : ""}
+                onClick={() => setActiveLink(index)}
+              >
+                <span>{linkData.linkName}</span>
+                <Icon className="nav-icon" />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
